242ccbbae445427b89f17e83b2e0065d684a2313
Jeong-dap <njd990603@naver.com>
2024-08-04 17:34:05 +0900

    before class registration

---
+++ src/Codeup_32.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Codeup_32 {
+    public static void main(String[] args) {
+        int i;
+        Scanner sc = new Scanner(System.in);
+
+        i = sc.nextInt();
+
+        System.out.printf("%x", i);
+    }
+}
